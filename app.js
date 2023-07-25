6374f0c6d21343ca0300722eb6bbc67ce884de48
unknown <SENA@ARMDFPCCIFSD974.FormacionSena.RED>
2023-07-25 09:47:07 -0500

    Busqueda finalizada.

--- app.js
+++ app.js
@@ -1,25 +1,80 @@
-const API_KEY = 'AIzaSyCAhcxb7VJrUoHIk6rOgiqoEbR9dAcssgk';
-const API_URL = 'https://api.example.com/endpoint';
+const API_KEY = "AIzaSyCAhcxb7VJrUoHIk6rOgiqoEbR9dAcssgk";
 
-// Función para hacer la solicitud a la API
-function fetchData() {
-  const xhr = new XMLHttpRequest();
-  
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        // Manejo de la respuesta de la API
-        console.log(xhr.responseText);
-      } else {
-        console.error('Error al obtener los datos:', xhr.statusText);
+// const API_URL = 'https://tenor.googleapis.com/v1/trending?q=';
+
+var SEARCH_URL = "https://tenor.googleapis.com/v2/search?q="
+const TRENDING_URL = "https://tenor.googleapis.com/v2/featured?key="
+
+const filterSalient = document.getElementById('filterSalient')
+
+const limit = 5;
+
+const generateBody = async () => {
+
+  // const URL = `${API_URL}&key=${API_KEY}&limit=${limit}`;
+
+  // await fetch(URL)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data.results);
+  //     for (let i = 0; i < limit; i++) {
+  //       let img = document.createElement('img')
+  //       img.src = data.results[i]["media_formats"]["nanogif"]["url"]
+  //       filterSalient.appendChild(img)
+  //       console.log(img);
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.error('Error al obtener los GIFs:', error);
+  //   });
+
+  let trendingURL = `${TRENDING_URL}${API_KEY}&limit=${limit}`
+
+  await fetch(trendingURL)
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < limit; i++) {
+        let img = document.createElement('img')
+        img.src = data.results[i]["media_formats"]["nanogif"]["url"]
+        filterSalient.appendChild(img)
       }
-    }
-  };
-  
-  xhr.open('GET', API_URL, true);
-  xhr.setRequestHeader('Authorization', `Bearer ${API_KEY}`);
-  xhr.send();
+    })
+    .catch(error => {
+      console.error('Error al obtener los GIFs:', error);
+    });
+
+
 }
 
-// Llamar a la función para obtener los datos
-fetchData();
+window.addEventListener("DOMContentLoaded", generateBody);
+
+
+
+function getData() {
+
+  let search = document.getElementById("search")
+
+  search.addEventListener('keydown', async function (event) {
+    if (event.keyCode === 13) {
+      filterSalient.innerHTML = ""
+      const searchWord = search.value;
+      searchWord.innerHTML = "";
+      let trendingURL = `${SEARCH_URL}${searchWord}&key=${API_KEY}&limit=${limit}`
+      await fetch(trendingURL)
+        .then(response => response.json())
+        .then(data => {
+          for (let i = 0; i < limit; i++) {
+            let img = document.createElement('img')
+            img.src = data.results[i]["media_formats"]["nanogif"]["url"]
+            filterSalient.appendChild(img)
+          }
+        })
+        .catch(error => {
+          console.error('Error al obtener los GIFs:', error);
+        });
+    }
+  });
+
+}
+
+getData()
